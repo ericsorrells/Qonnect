3cbe607dc3c3239fbdddff1c8caa5447eb8ff4f4
Eric Sorrells <ericsorrells@hotmail.com>
2018-04-14 22:36:13 -0400

    Working Tests

--- src/components/Home/__test__/Home.test.js
+++ src/components/Home/__test__/Home.test.js
@@ -3,6 +3,6 @@ import { shallow } from 'enzyme';
 import Home from '../Home';
 
 test('testing', () => {
-  component = shallow(<Home />);
-  expect(component).toHaveText('Text');
+  const component = shallow(<Home />);
+  expect(component).toIncludeText('hello');
 });
